1779dfed8fb42af313cbf5c013e60513823066bb
kjfsoul <kjfsoul@gmail.com>
2025-10-26 01:22:20 -0400

    hotfix: fix TSX syntax; iframe tracker live

--- code_artifacts/3iatlas-flight-tracker/frontend/src/main.tsx
+++ code_artifacts/3iatlas-flight-tracker/frontend/src/main.tsx
@@ -7,11 +7,11 @@ const TRACKER_URL =
 
 function App() {
   return (
-    <div style={{position:"fixed", inset:0, background:"black"}}>
+    <div style={{ position: "fixed", inset: 0, background: "black" }}>
       <iframe
         src={TRACKER_URL}
         title="3I/ATLAS Tracker"
-        style={{width:"100%", height:"100%", border:"0", display:"block", background:"black"}}
+        style={{ width: "100%", height: "100%", border: "0", display: "block", background: "black" }}
         allow="accelerometer; gyroscope; fullscreen"
         referrerPolicy="no-referrer"
       />
